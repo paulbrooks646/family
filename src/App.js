dde6f3fa4766ef9f8e798e2a7bc7d366169aceec
paulbrooks646 <paulbrooks646@gmail.com>
2021-06-29 16:53:46 -0600

    nav start

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.scss';
 function App() {
   return (
     <div>
+      <nav>
+        <img className="profile-image" src=""/>
+        <h3 className="profile-name">Paul Brooks</h3>
+      </nav>
       <h1>This is the start of something new!</h1>
     </div>
   );
