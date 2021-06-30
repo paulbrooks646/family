d27019eb78ce0298c2fbcbfd54b92a107919f000
paulbrooks646 <paulbrooks646@gmail.com>
2021-06-30 14:06:37 -0600

    index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./App";
+import { HashRouter, BrowserRouter } from "react-redux";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+const Router =
+  process.env.NODE_ENV === "development" ? HashRouter : BrowserRouter;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
